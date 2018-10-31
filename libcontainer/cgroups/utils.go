kolyshkin/runc

commit e7b57cb042130edf86506d189734018edc3f2c18

Ignore cgroup2 mountpoints
Our current cgroup parsing logic assumes cgroup v1 mounts
so we should ignore cgroup2 mounts for now

Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

libcontainer/cgroups/utils.go
@@ -149,7 +149,7 @@ func getCgroupMountsHelper(ss map[string]bool, mi io.Reader, all bool) ([]Mount,
 		if sepIdx == -1 {
 			return nil, fmt.Errorf("invalid mountinfo format")
 		}
-		if txt[sepIdx+3:sepIdx+9] != "cgroup" {
+		if txt[sepIdx+3:sepIdx+10] == "cgroup2" || txt[sepIdx+3:sepIdx+9] != "cgroup" {
 			continue
 		}
 		fields := strings.Split(txt, " ")

commit 8470aa2042008a87e212e2ce39b701e0e4c624fb

getCgroupMountsHelper: simplify/strentghen a check
There is no need to have a special condition for 'cgroup2', the code
is only interested in 'cgroup'. This makes the code more readable,
a tad faster, and should also fix cases (theoretical for now) of
of 'cgroup3', 'cgroupWhatever' etc.

Fixes: e7b57cb042130edf86

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/cgroups/utils.go
@@ -142,7 +142,7 @@ func getCgroupMountsHelper(ss map[string]bool, mi io.Reader, all bool) ([]Mount,
 		if sepIdx == -1 {
 			return nil, fmt.Errorf("invalid mountinfo format")
 		}
-		if txt[sepIdx+3:sepIdx+10] == "cgroup2" || txt[sepIdx+3:sepIdx+9] != "cgroup" {
+		if txt[sepIdx+3:sepIdx+10] != "cgroup " {
 			continue
 		}
 		fields := strings.Split(txt, " ")