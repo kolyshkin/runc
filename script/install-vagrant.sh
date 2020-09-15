fb0d7f2a58b1d5c0bfef3fd9474a8611612cec5d
Kir Kolyshkin <kolyshkin@gmail.com>
2020-09-14 20:03:18 -0700

    scripts/install-vagrant.sh: nits
     - move the deb file name to $DEB;
     - use quotes everywhere (shellcheck-approved);
     - install $DEB together with all other packages.
    
    The last item speeds things up a little bit, and could have been useful
    if the package would list the correct dependencies -- alas it's not, but
    it might be in the future.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- script/install-vagrant.sh
+++ script/install-vagrant.sh
@@ -3,9 +3,18 @@ set -eux -o pipefail
 VAGRANT_VERSION="2.2.7"
 
 # https://github.com/alvistack/ansible-role-virtualbox/blob/6887b020b0ca5c59ddb6620d73f053ffb84f4126/.travis.yml#L30
+DEB="vagrant_${VAGRANT_VERSION}_$(uname -m).deb"
+wget "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/$DEB"
 apt-get update
-apt-get install -q -y bridge-utils dnsmasq-base ebtables libvirt-bin libvirt-dev qemu-kvm qemu-utils ruby-dev
-wget https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_$(uname -m).deb
-dpkg -i vagrant_${VAGRANT_VERSION}_$(uname -m).deb
-rm -f vagrant_${VAGRANT_VERSION}_$(uname -m).deb
+apt-get install -q -y \
+	bridge-utils \
+	dnsmasq-base \
+	ebtables \
+	libvirt-bin \
+	libvirt-dev \
+	qemu-kvm \
+	qemu-utils \
+	ruby-dev \
+	./"$DEB"
+rm -f ./"$DEB"
 vagrant plugin install vagrant-libvirt
