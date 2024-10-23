4257fb4b70bb14da2580c06d51f03c41fac66c33
Kir Kolyshkin <kolyshkin@gmail.com>
2024-10-23 16:28:42 -0700

    libct/cg: switch to libct/cg/configs
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libcontainer/cgroups/devices/systemd_test.go
+++ libcontainer/cgroups/devices/systemd_test.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/opencontainers/runc/internal/testutil"
 	"github.com/opencontainers/runc/libcontainer/cgroups"
+	"github.com/opencontainers/runc/libcontainer/cgroups/configs"
 	"github.com/opencontainers/runc/libcontainer/cgroups/systemd"
-	"github.com/opencontainers/runc/libcontainer/configs"
 	"github.com/opencontainers/runc/libcontainer/devices"
 )
 
