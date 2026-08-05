1a67c080f572e50eb223c5dce7cf8053bf1cc4aa
Kir Kolyshkin <kolyshkin@gmail.com>
2026-08-05 13:02:35 -0700

    TEST: add script with deliberate shellcheck problems
    Baseline run: no problem matcher registered at all, to see what (if
    anything) GitHub annotates on its own.
    
    NOT FOR MERGING.

---
+++ script/zz-shellcheck-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Temporary file with deliberate shellcheck issues, used to see which
+# findings (if any) GitHub annotates. NOT FOR MERGING.
+
+foo=$1
+echo $foo
+
+echo $(date)
+
+cd /tmp
